4c68ecc9e96ae651551891c39597d342197ddab9
Gabriel Santos Pole <54583922+GabrielPole@users.noreply.github.com>
2020-05-11 21:07:29 -0300

    Demo-funcionando

--- js/index.js
+++ js/index.js
@@ -1,2 +1,38 @@
 var button = document.querySelectorAll("button");
- console.log(button[4])
+var jogo = document.querySelector("ul");
+
+jogo.addEventListener("click", function(event){
+    event.preventDefault();
+    var xx = event.target;
+
+    console.log(xx.textContent)
+
+    if(xx.textContent.length > 1){
+        return
+    }
+
+    xx.textContent = "X";
+    xx.style.color = "#098ca3";
+    
+})
+
+jogo.addEventListener("contextmenu", function(event){
+    event.preventDefault();
+    var oo = event.target;
+
+    if(oo.textContent.length > 1){
+        return
+    }
+
+    oo.textContent = "O";
+    oo.style.color = "#1cb87c"
+    
+})
+
+
+
+    /*var casa = casa;
+    casa = button[2].textContent;
+            098ca3
+    console.log(casa)
+    fun*/
